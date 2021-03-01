6bd1020733c348ba6d1876d7962bcc7e42a34782
Carlo Ciarnelli <ciarnellicarloedoardo@gmail.com>
2021-03-01 18:49:58 +0100

    Third commit, adding bookmark and bookmark styles

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+
+
 const form = document.querySelector("#bookform")
 
-let popi = e => {
+let setBookmark = e => {
     let websiteName = document.querySelector('#websitename').value;
     let websiteUrl = document.querySelector('#websiteurl').value;
 
@@ -9,8 +12,33 @@ let popi = e => {
         url: websiteUrl
     }
 
-    console.log(bookmark);
+    if(localStorage.getItem('bookmarks') === null) {
+        let bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+
     e.preventDefault();
 }
 
-form.addEventListener('submit', popi);
+function outputBookmarks() {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    console.log(bookmarks);
+    let bookmarksResults = document.querySelector("#websitesbooked");
+    bookmarksResults.innerHTML = "";
+    for(let i = 0; i < bookmarks.length; i++){
+        let name = bookmarks[i].name;
+        let url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="savedBookmarks">'+
+                                      '<h3>' +name+
+                                      '</h3>'+
+                                      '</div>';
+    }
+}
+
+form.addEventListener('submit', setBookmark);
