39f4dec4395f4950bceadc805170da1d7a00a832
Carlo Ciarnelli <ciarnellicarloedoardo@gmail.com>
2021-03-02 10:46:53 +0100

    Fourth commit, remove bookmark

--- script.js
+++ script.js
@@ -22,12 +22,25 @@ let setBookmark = e => {
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
+    outputBookmarks()
+
     e.preventDefault();
+}
+
+function deleteBookmark(url) {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for (let i = 0; i < bookmarks.length; i++) {
+        if(bookmarks[i].url == url) {
+            bookmarks.splice(i, 1)
+        }
+    }
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    outputBookmarks()
 }
 
 function outputBookmarks() {
     let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    console.log(bookmarks);
     let bookmarksResults = document.querySelector("#websitesbooked");
     bookmarksResults.innerHTML = "";
     for(let i = 0; i < bookmarks.length; i++){
@@ -37,6 +50,8 @@ function outputBookmarks() {
         bookmarksResults.innerHTML += '<div class="savedBookmarks">'+
                                       '<h3>' +name+
                                       '</h3>'+
+                                      '<a class="visit" target="_blank" href="'+url+'">Visit</a>'+
+                                      '<a onclick="deleteBookmark(\''+url+'\')" class="delete" href="#">Delete</a>'+
                                       '</div>';
     }
 }
